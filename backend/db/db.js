d9e75dd480458307fdfffe72af8906e470627bf3
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-27 21:09:39 +0545

    index .js

---
+++ backend/db/db.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST||'localhost',
+    user: process.env.DB_USER|| 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME ||'hariyalighar'
+});
+module.exports = db.promise();
---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const db = require('./db/db');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+db.query("SELECT 1")
+    .then(() => {
+        console.log("Database Connected Successfully.");
+                app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("Database Connection Failed.", err);
+    });
+
+app.get('/', (req, res) => {
+    res.send("Backend server is running successfully.");
+});
