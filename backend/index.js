e211518d0a74276b69ddc454e9d32c75d88130bc
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-30 07:51:23 +0545

    Add authRoute middleware for authentication-related API endpoints

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ const cors = require('cors');
 require('dotenv').config();
 const db = require('./db/db');
 const productRoute = require('./routes/productRoute');
+const authRoute=require('./routes/authRoute');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/images', express.static('image'));
 app.use('/api/products', productRoute);
+app.use('/api/auth',authRoute);
 
 const PORT = process.env.PORT || 5000;
 
