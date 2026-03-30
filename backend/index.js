4995272c4927925a3496bf4ab5c923b1ca938b6c
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-30 17:23:54 +0545

    Refactor routes to use updated product and authentication route files

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const db = require('./db/db');
-const productController = require('./controllers/productController');
-const authController=require('./controllers/authController');
+const productRoute = require('./routes/productRoute');
+const authRoute = require('./routes/authRoute');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/images', express.static('image'));
-app.use('/api/products', productController);
-app.use('/api/auth',authController);
+app.use('/api/products', productRoute);
+app.use('/api/auth', authRoute);
 
 const PORT = process.env.PORT || 5000;
 
