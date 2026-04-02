a72015d5658488b23e905934dbe83d07108b9c5e
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-04-02 15:28:20 +0545

    Add token verification route with authMiddleware

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,8 +1,16 @@
-const express=require('express');
-const router=express.Router();
-const authController=require('../controllers/authController');
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const { authMiddleware } = require('../middleware/authMiddleware');
 
 router.post('/signup', authController.signup);
 
 router.post('/login', authController.login);
+
+router.get('/tokenverification', authMiddleware, (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "token validate successfull"
+    })
+});
 module.exports = router;
