be53d8ee615382f6c54d2f39e14a22e22ad4f48b
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-04-20 08:06:42 +0545

    Add initiateEsewaPayment function to handle eSewa payment processing

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,1 +1,16 @@
 const crypto = require('crypto');
+
+exports.initiateEsewaPayment = async (req, res) => {
+    try {
+        const { amount, orderId } = req.body;
+        // eSewa Test Credentials
+        const product_code = "EPAYTEST";
+        const secretKey = "8g8M9DGb22302682";
+        const transaction_uuid = `${orderId}-${Date.now()}`;
+
+        const data = `total_amount=${amount},transaction_uuid=${transaction_uuid},product_code=${product_code}`;
+    }
+    catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+};
