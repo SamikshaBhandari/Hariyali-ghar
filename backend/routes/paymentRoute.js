77809e8e46eb99a34a253e0da9c3400f96e97fd4
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-04-21 08:20:01 +0545

    Add eSewa payment initiation route with authentication middleware

--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const paymentController = require('../controllers/paymentController');
 const { authMiddleware } = require('../middleware/authMiddleware');
 
-
+router.post('/esewa-initiate', authMiddleware, paymentController.initiateEsewaPayment);
 module.exports = router;
