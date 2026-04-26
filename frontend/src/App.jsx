e01b369bda9384359684c9723dc96dc34afe0e14
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-04-26 21:32:15 +0545

    initialize frontend structure with main components and Tailwind CSS integration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+    return (
+        <div className="bg-slate-900 min-h-screen">
+            <Navbar />
+
+            <div className="flex items-center justify-center h-screen">
+            </div>
+        </div>
+    );
+}
+
+export default App;
