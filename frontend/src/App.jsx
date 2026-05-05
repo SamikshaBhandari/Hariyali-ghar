59ff2a287313bbe84eda3404e8dfa1d9e35e3461
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-05-05 17:47:05 +0545

    routing for Register pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import Register from './pages/Register';
 
 function App() {
     return (
-        <>
+        <div className="flex flex-col min-h-screen">
             <Navbar />
-            <Home />
+
+            <main className="flex-grow">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </main>
+
             <Footer />
-        </>
+        </div>
     );
 }
 
