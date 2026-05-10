c2c860bd6bdcdfdc6bbec52b8120e6a6e8ad575c
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-05-10 07:07:20 +0545

    add admindashboard

---
+++ frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminDashboard = () => {
+    return (
+        <div className="pt-32 text-center">
+            <h1 className="text-3xl font-bold text-green-800">Welcome to Admin Dashboard</h1>
+        </div>
+    );
+};
+
+export default AdminDashboard;
