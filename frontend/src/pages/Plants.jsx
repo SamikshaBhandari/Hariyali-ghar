646a8b6aa915b82959ed00f04017a79de8077625
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-05-12 16:58:14 +0545

    add fetchFilteredPlants function to retrieve plants based on search query and category

--- frontend/src/pages/Plants.jsx
+++ frontend/src/pages/Plants.jsx
@@ -8,5 +8,20 @@ const Plants = () => {
     const [activeCategory, setActiveCategory] = useState("All");
     const categories = ["All", "Indoor Plants", "Outdoor Plants", "Seed Plants", "Succulents Plants"];
 
-}
+    const fetchFilteredPlants = async () => {
+        try {
+            const response = await axios.get(`http://localhost:5000/api/productds/filter`, {
+                params: {
+                    search: searchQuery,
+                    category: activeCategory
+                }
+            });
+            if (response.data.success) {
+                setPlants(response.data.data);
+            }
+        } catch (err) {
+            console.error("Error fetching filtered plants:", error);
+        }
+    }
+};
 export default Plants;
