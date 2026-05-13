41b53f3a534271f854bcdf068a9930ed36cfdc42
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-05-13 15:57:29 +0545

    add header part

--- frontend/src/pages/Plants.jsx
+++ frontend/src/pages/Plants.jsx
@@ -6,11 +6,11 @@ const Plants = () => {
     const [plants, setPlants] = useState([]);
     const [searchQuery, setSearchQuery] = useState("");
     const [activeCategory, setActiveCategory] = useState("All");
-    const categories = ["All", "Indoor Plants", "Outdoor Plants", "Seed Plants", "Succulents Plants"];
+    const categories = ["All", "Indoor Plants", "Outdoor Plants", "Seed Plant", "Succulents Plant"];
 
     const fetchFilteredPlants = async () => {
         try {
-            const response = await axios.get(`http://localhost:5000/api/productds/filter`, {
+            const response = await axios.get(`http://localhost:5000/api/products/filter`, {
                 params: {
                     search: searchQuery,
                     category: activeCategory
@@ -20,19 +20,26 @@ const Plants = () => {
                 setPlants(response.data.data);
             }
         } catch (err) {
-            console.error("Error fetching filtered plants:", error);
+            console.error("Error fetching filtered plants:", err);
         }
-    }
+    };
+
+    useEffect(() => {
+        fetchFilteredPlants();
+    }, [searchQuery, activeCategory]);
+
+    return (
+        <div className="min-h-screen bg-gray-50 pt-20">
+
+            {/* header section */}
+            <div className="bg-green-700 py-10 px-8 text-white text-start">
+                <h1 className="text-3xl font-bold mb-2 tracking-tight">Hariyali Ghar Collection</h1>
+                <p className="text-gray-100 opacity-90">Find the perfect green companion for your space</p>
+            </div>
+
+
+        </div>
+    );
 };
 
-useEffect(() => {
-    fetchFilteredPlants();
-}, [searchQuery, activeCategory]);
-
-
-return (
-    <>
-
-    </>
-)
 export default Plants;
