324d4a078fbe11440ee380d65b92fbc1f7c4ab34
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-06-04 21:00:21 +0545

    add route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,8 @@ import AdminRoute from './components/AdminRoute';
 import ManagePlants from './pages/ManagePlants';
 import AddPlant from './pages/AddPlant';
 import AdminOrder from './pages/AdminOrder';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 
 
 function App() {
@@ -45,6 +47,8 @@ function App() {
                     <Route path="/admin/manage-plants" element={<AdminRoute><ManagePlants /></AdminRoute>} />
                     <Route path="/admin/add-plant" element={<AdminRoute><AddPlant /></AdminRoute>} />
                     <Route path="/admin/orders" element={<AdminRoute><AdminOrder /></AdminRoute>} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path="/reset-password" element={<ResetPassword />} />
                 </Routes>
             </main>
 
