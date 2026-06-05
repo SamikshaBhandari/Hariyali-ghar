3def44c2e3dca87746853a02a3bcdfb043f4ad5f
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-06-05 21:00:48 +0545

    add forgot password and reset password routes to authRoute

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -16,4 +16,7 @@ router.get('/tokenverification', authMiddleware, (req, res) => {
 
 router.post('/emailverification', authController.verifyEmail);
 
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
+
 module.exports = router;
