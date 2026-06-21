dcaf9e1e54e8d9be7855258560b7b96e380eb8a9
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-06-21 09:26:40 +0545

    fix route name

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ const db = require('../db/db');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const sendEmail = require('../utils/sendemail');
+const sendEmail = require('../utils/sendEmail');
 
 //Signup Logic
 exports.signup = async (req, res) => {
