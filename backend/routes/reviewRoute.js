52ff8854c91fd0930446de08c0bfbc4a7870dc94
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-09-21 17:22:58 +0545

    remove unnecessary newline in reviewRoute.js

--- backend/routes/reviewRoute.js
+++ backend/routes/reviewRoute.js
@@ -4,7 +4,6 @@ const reviewController = require('../controllers/reviewController');
 const { authMiddleware } = require('../middleware/authMiddleware');
 
 router.post('/add', authMiddleware, reviewController.addReview);
-
 router.get('/product/:product_id', reviewController.getProductReviews);
 
 module.exports = router;
